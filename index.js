813e8fa7a0292774052163eef1c434d8f3cc78d4
shuson <wlgq@hotmail.com>
2014-09-05 17:03:12 +0800

    using wx module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var express = require('express')
+var express = require('express');
 var app = express();
-var sha1 = require('sha1')
+var sha1 = require('sha1');
+var weixin = require('./Weixin');
 
 var token = 'wxapiAtDigitalOcean';
 
@@ -9,7 +10,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res) {
 	console.log(req.query.echostr)
-    if (checkSignature(req)) {
+    if (weixin.checkSignature(req)) {
         res.send(200, req.query.echostr);
     } else {
         res.send(200, 'fail');
@@ -17,31 +18,8 @@ app.get('/', function(req, res) {
 });
 
 app.get('/helloworld',function(req,res){
-
 	res.send("hello world!");
 });
-
-var checkSignature = function(req) {    		
-	
-	// get params
-	var signature = req.query.signature;
-	var timestamp = req.query.timestamp;
-	var nonce = req.query.nonce;
-	var echostr = req.query.echostr;
-	
-	// sort by dict order
-	var array = [token, timestamp, nonce];
-	array.sort();
-	console.log(array)
-	// cat them
-	var str = sha1(array.join(""));
-	console.log(str)
-	if(str == signature) {
-		return true;
-	} else {
-		return false;
-	}
-}
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
